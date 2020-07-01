3d4abee18f602d9b94cb41950883a99de93f94b3
Aliaksandr Rykau <Aliaksandr_Rykau@epam.com>
2020-07-01 19:58:06 +0300

    Add project files.

---
+++ LinqMethods.Tests/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using NUnit.Framework;
+
+namespace LinqMethods.Tests
+{
+    [TestFixture]
+    public class Class1Tests
+    {
+        [Test]
+        public void GetFruits_ReturnsValidDate()
+        {
+            var c = new Class1();
+            var result = c.GetFruits(new []
+            {
+                (true, "apple"), (false, "Moscow"), (true, "banana"), (false, "Kiev"), (true, "lemon"), (false, "Minsk")
+            });
+
+            Assert.Contains("apple", (ICollection)result);
+            Assert.Contains("banana", (ICollection)result);
+            Assert.Contains("lemon", (ICollection)result);
+        }
+
+        [Test]
+        public void GetFruits_UsesLinq()
+        {
+            var c = new Class1();
+            var expr = c.GetFruits2();
+
+            var methodText = expr.ToString(); // == "k => k.Where(i => i.Item1).Select(i => i.Item2).ToArray()"
+            bool hasSelectCall = methodText.Contains("Select");
+            bool hasWhereCall = methodText.Contains("Where");
+
+            Assert.IsTrue(hasSelectCall);
+            Assert.IsTrue(hasWhereCall);
+
+            var del = expr.Compile();
+            var result = del(new[]
+            {
+                (true, "apple"), (false, "Moscow"), (true, "banana"), (false, "Kiev"), (true, "lemon"), (false, "Minsk")
+            });
+
+            Assert.Contains("apple", (ICollection)result);
+            Assert.Contains("banana", (ICollection)result);
+            Assert.Contains("lemon", (ICollection)result);
+        }
+    }
+}
---
+++ LinqMethods/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace LinqMethods
+{
+    public class Class1
+    {
+        public IEnumerable<string> GetFruits(IEnumerable<(bool, string)> input)
+        {
+            return input.Where(i => i.Item1).Select(i =>i.Item2).ToArray();
+        }
+ 
+        public Expression<Func<IEnumerable<(bool, string)>, IEnumerable<string>>> GetFruits2()
+        {
+            Expression<Func<IEnumerable<(bool, string)>, IEnumerable<string>>> del = k => k.Where(i => i.Item1).Select(i => i.Item2).ToArray();
+
+            return del;
+        }
+    }
+}
